34ba8388ed1c2ad2cf1104f1a6ffe3de5974e02b
Javier Illana <fjillana@gmail.com>
2025-10-02 15:03:30 +0000

    donde puedo descargarme los archivos que mencionas?

--- src/components/student/center-data-form.tsx
+++ src/components/student/center-data-form.tsx
@@ -123,3 +123,4 @@ export function CenterDataForm({ disabled = false, selectedActions: initialSelec
 }
 
     
+    
--- src/hooks/useStudentGame.tsx
+++ src/hooks/useStudentGame.tsx
@@ -353,3 +353,5 @@ export function useStudentGame() {
   }
   return context;
 }
+
+    

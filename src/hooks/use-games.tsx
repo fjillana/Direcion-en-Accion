39e28926d4e0a1be6443426c4d4e492ce51eaedf
Javier Illana <fjillana@gmail.com>
2025-10-04 16:33:55 +0000

    Try fixing this error: `Console Error: FirebaseError: Missing or insuffi

--- src/hooks/use-games.tsx
+++ src/hooks/use-games.tsx
@@ -157,18 +157,18 @@ export function GamesProvider({ children }: { children: ReactNode }) {
   const removeGame = async (gameId: string) => {
     if (!firestore) return;
     const gameDocRef = doc(firestore, "games", gameId);
-    try {
-      await deleteDoc(gameDocRef);
-      await refreshGames();
-    } catch (serverError) {
-      const permissionError = new FirestorePermissionError({
-          path: gameDocRef.path,
-          operation: 'delete',
+    
+    deleteDoc(gameDocRef)
+      .then(() => {
+        refreshGames();
+      })
+      .catch(async (serverError) => {
+        const permissionError = new FirestorePermissionError({
+            path: gameDocRef.path,
+            operation: 'delete',
         });
-      errorEmitter.emit('permission-error', permissionError);
-      // Re-throw the original error if you want the caller to be able to catch it too
-      throw serverError;
-    }
+        errorEmitter.emit('permission-error', permissionError);
+    });
   };
 
   const updateGame = async (gameId: string, updatedGame: Partial<Game>) => {
@@ -360,3 +360,5 @@ export function useGames() {
   }
   return context;
 }
+
+    
